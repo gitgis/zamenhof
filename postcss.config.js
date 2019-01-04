546e3905698126a65827b33caab02513820def9c
Grzegorz Godlewski <gg@gitgis.com>
2019-01-04 13:57:45 +0100

    Fix CSS sourcemaps

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,16 @@
-module.exports = {
-    plugins: {
-        'postcss-import': {
-            path: 'assets/css'
-        },
-        'postcss-cssnext': {
-            browsers: ['last 2 versions', '> 5%'],
-        },
-        'postcss-reporter': {
-            clearReportedMessages: true
+module.exports = ctx => {
+    return {
+        map: ctx.options.map,
+        plugins: {
+            'postcss-import': {
+                path: 'assets'
+            },
+            'postcss-cssnext': {
+                browsers: ['last 2 versions', '> 5%'],
+            },
+            'postcss-reporter': {
+                clearReportedMessages: true
+            }
         }
     }
 };
--- upload.sh
+++ upload.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+rm public/* -rf
 hugo
 aws s3 sync ./public/ s3://zamenhof.gitgis.com/ --delete --acl public-read
